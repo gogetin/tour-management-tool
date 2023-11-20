d1bbe13b9c23e7ce44f6c68babea77130054d51b
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2023-11-20 19:18:11 +0530

    update

--- app/(protected)/packages/details/page.jsx
+++ app/(protected)/packages/details/page.jsx
@@ -1,30 +1,91 @@
 "use client";
 
-import { Container, Toolbar } from "@mui/material";
 import React from "react";
-import { Paper, TextField, Typography, Box, IconButton } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import MenuIcon from "@mui/icons-material/Menu";
-import SaveIcon from "@mui/icons-material/Save";
-import PrintIcon from "@mui/icons-material/Print";
-import ShareIcon from "@mui/icons-material/Share";
-
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
+import HotelIcon from "@mui/icons-material/Hotel";
+import AirportShuttleIcon from "@mui/icons-material/AirportShuttle";
+import NaturePeopleIcon from "@mui/icons-material/NaturePeople";
+import AttractionsIcon from "@mui/icons-material/Attractions";
+import {
+  Paper,
+  TextField,
+  Typography,
+  Box,
+  IconButton,
+  CardActionArea,
+  CardActions,
+  Button,
+} from "@mui/material";
+import { useForm, useFieldArray } from "react-hook-form";
 
 function Page() {
   const actions = [
     {
-      icon: <SaveIcon />,
+      icon: <HotelIcon />,
       name: "Add Hotel",
     },
-    { icon: <PrintIcon />, name: "Add Transport" },
-    { icon: <ShareIcon />, name: "Add Attraction" },
+    { icon: <AirportShuttleIcon />, name: "Add Transport" },
+    { icon: <AttractionsIcon />, name: "Add Attraction" },
   ];
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: {
+      // Set your initial form values here
+      hotels: [
+        {
+          name: "",
+          Address: "",
+          checkin: "",
+          checkout: "",
+          purchasePrice: 0,
+          sellPrice: 0,
+        },
+      ],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "hotels",
+  });
+  const onSubmit = (data) => {
+    // Handle form submission logic here
+    console.log(data);
+  };
   return (
     <>
-      <Typography>Hello </Typography>
+      <Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {fields.map((group, groupIndex) => (
+            <div key={group.id}>
+              <input
+                {...register(`groups[${groupIndex}].name`)}
+                defaultValue={group.firstName} // use defaultValue for default values
+                placeholder="First Name"
+              />
+              <input
+                {...register(`groups[${groupIndex}].lastName`)}
+                defaultValue={group.lastName} // use defaultValue for default values
+                placeholder="Last Name"
+              />
+              <button type="button" onClick={() => remove(groupIndex)}>
+                Remove Group
+              </button>
+            </div>
+          ))}
+
+          <button
+            type="button"
+            onClick={() => append({ firstName: "", lastName: "" })}
+          >
+            Add Group
+          </button>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Box>
+
       <SpeedDial
         ariaLabel="SpeedDial basic example"
         sx={{ position: "absolute", bottom: 16, right: 16 }}
--- app/(protected)/packages/page.jsx
+++ app/(protected)/packages/page.jsx
@@ -17,58 +17,95 @@ import {
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function Page() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <Box sx={{ padding: 4 }}>
-      <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        sx={{ padding: 2 }}
-      >
-        <Typography variant="h6">My Packages</Typography>
-        <TextField
-          size="small"
-          id="outlined-basic"
-          label="Search"
-          variant="outlined"
-        />
-      </Box>
-      <Paper sx={{ padding: 4, minHeight: "100%" }}>
-        <Box>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="/images/card_img_demo.jpg"
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Singapore
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div">
-                  Buyer: Discovery
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Add Details
-              </Button>
-            </CardActions>
-          </Card>
+    <>
+      <Box sx={{ padding: 4 }}>
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          sx={{ padding: 2 }}
+        >
+          <Typography variant="h6">My Packages</Typography>
+          <TextField
+            size="small"
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+          />
         </Box>
-      </Paper>
-      <Fab
-        sx={{ position: "absolute", bottom: 16, right: 16 }}
-        color="primary"
-        aria-label="add"
-      >
-        <AddIcon />
-      </Fab>
-    </Box>
+        <Paper sx={{ padding: 4, minHeight: "100%" }}>
+          <Box>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="/images/card_img_demo.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Singapore
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    Buyer: Discovery
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <a href="packages/details">
+                  <Button size="small" color="primary">
+                    Add Details
+                  </Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Box>
+        </Paper>
+        <Fab
+          sx={{ position: "absolute", bottom: 16, right: 16 }}
+          color="primary"
+          aria-label="add"
+          onClick={handleClickOpen}
+        >
+          <AddIcon />
+        </Fab>
+      </Box>
+      <Dialog fullWidth open={open} onClose={handleClose}>
+        <DialogTitle>Enter Package Details</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="pgkg_name"
+            label="Package Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Create</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
