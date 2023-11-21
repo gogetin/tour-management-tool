005fe95648990be780cfc8547e402c2fa97bd3bc
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2023-11-21 18:35:09 +0530

    details page wip

--- app/(protected)/packages/details/page.jsx
+++ app/(protected)/packages/details/page.jsx
@@ -21,9 +21,25 @@ import {
 import { useForm, useFieldArray } from "react-hook-form";
 
 function Page() {
+  function handleAddHotel() {
+    append({
+      name: "",
+      address: "",
+      checkin: "",
+      checkout: "",
+      purchasePrice: 0,
+      sellPrice: 0,
+    });
+  }
+  function handleAddTransport() {}
+  function handleRemove(e) {
+    let id = parseInt(e.target.dataset.id);
+    console.log(id);
+    remove(id);
+  }
   const actions = [
     {
-      icon: <HotelIcon />,
+      icon: <HotelIcon onClick={handleAddHotel} />,
       name: "Add Hotel",
     },
     { icon: <AirportShuttleIcon />, name: "Add Transport" },
@@ -31,23 +47,13 @@ function Page() {
   ];
   const { register, control, handleSubmit } = useForm({
     defaultValues: {
-      // Set your initial form values here
-      hotels: [
-        {
-          name: "",
-          Address: "",
-          checkin: "",
-          checkout: "",
-          purchasePrice: 0,
-          sellPrice: 0,
-        },
-      ],
+      groups: [],
     },
   });
 
   const { fields, append, remove } = useFieldArray({
     control,
-    name: "hotels",
+    name: "groups",
   });
   const onSubmit = (data) => {
     // Handle form submission logic here
@@ -55,35 +61,109 @@ function Page() {
   };
   return (
     <>
-      <Box>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {fields.map((group, groupIndex) => (
-            <div key={group.id}>
-              <input
-                {...register(`groups[${groupIndex}].name`)}
-                defaultValue={group.firstName} // use defaultValue for default values
-                placeholder="First Name"
-              />
-              <input
-                {...register(`groups[${groupIndex}].lastName`)}
-                defaultValue={group.lastName} // use defaultValue for default values
-                placeholder="Last Name"
-              />
-              <button type="button" onClick={() => remove(groupIndex)}>
-                Remove Group
-              </button>
-            </div>
-          ))}
-
-          <button
-            type="button"
-            onClick={() => append({ firstName: "", lastName: "" })}
+      <Box display={"flex"} justifyContent={"center"}>
+        <Paper sx={{ width: "100%" }}>
+          <Box
+            width={"80%"}
+            display={"flex"}
+            justifyContent={"end"}
+            padding={2}
+            maxWidth={"1200px"}
           >
-            Add Group
-          </button>
-
-          <button type="submit">Submit</button>
-        </form>
+            <Button variant="contained">Get Quatation</Button>
+          </Box>
+        </Paper>
+        <Box
+          padding={4}
+          sx={{
+            position: "fixed",
+            overflowY: "scroll",
+            width: "80%",
+            height: "60vh",
+            marginTop: 14,
+            maxWidth: "1200px",
+          }}
+          bgcolor={"#eff5ff"}
+        >
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {fields.map((group, groupIndex) => (
+              <Box
+                key={group.id}
+                style={{ height: "100%" }}
+                display={"flex"}
+                flexDirection={"column"}
+                marginBottom={5}
+              >
+                <Paper>
+                  <Box
+                    bgcolor={"#e8eef1"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                    padding={1}
+                  >
+                    <Typography>Hotel</Typography>
+                    <Button
+                      color="error"
+                      data-id={groupIndex}
+                      onClick={handleRemove}
+                    >
+                      Remove
+                    </Button>
+                  </Box>
+                  <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    alignContent={"center"}
+                    rowGap={2}
+                    padding={4}
+                  >
+                    <TextField
+                      label="Hotel Name"
+                      {...register(`groups[${groupIndex}].name`)}
+                      defaultValue={group.firstName}
+                      placeholder="Hotel Name"
+                      size="small"
+                    />
+                    <TextField
+                      {...register(`groups[${groupIndex}].address`)}
+                      defaultValue={group.address}
+                      placeholder="Hotel Address"
+                      size="small"
+                    />
+                    <TextField
+                      {...register(`groups[${groupIndex}].checkin`)}
+                      defaultValue={group.checkin}
+                      placeholder="Checkin Date"
+                      size="small"
+                    />
+                    <TextField
+                      {...register(`groups[${groupIndex}].checkout`)}
+                      defaultValue={group.checkin}
+                      placeholder="Checkout Date"
+                      size="small"
+                    />
+                    <TextField
+                      {...register(`groups[${groupIndex}].sellPrice`)}
+                      defaultValue={group.checkin}
+                      placeholder="Sell Price"
+                      size="small"
+                    />
+                    <TextField
+                      {...register(`groups[${groupIndex}].purchasePrice`)}
+                      defaultValue={group.checkin}
+                      placeholder="Purchase Price"
+                      size="small"
+                    />
+                    <Button size="small" variant="contained" type="button">
+                      Save
+                    </Button>
+                  </Box>
+                </Paper>
+              </Box>
+            ))}
+          </form>
+        </Box>
       </Box>
 
       <SpeedDial
